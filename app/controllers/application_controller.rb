acbb586cf757e7059b6da2532dac5724fa725e3c
tmhs0319 <110654977+tmhs0319@users.noreply.github.com>
2022-09-28 14:53:10 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,11 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
+<<<<<<< Updated upstream
       username == 'admin' && password == '2222'
+=======
+      username == 'tmhs1025' && password == 'tmhs1025'
+>>>>>>> Stashed changes
     end
   end
 end
